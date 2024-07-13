c881149723c80606edfa6176cef587e35a8ed5ca
zainaraza43 <urboyzain@gmail.com>
2024-07-13 18:40:15 -0400

    nvm idgaf about code "smells"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 plugins {
     kotlin("jvm") version "2.0.0"
-
-    // KTLint
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
-
-    // Detekt
-    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
 group = "ca.razacapital"
@@ -29,12 +23,4 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
-}
-
-tasks.register<Copy>("copyPreCommitHook") {
-    description = "Copy pre-commit git hook from the scripts to the .git/hooks folder."
-    group = "git hooks"
-    outputs.upToDateWhen { false }
-    from("$rootDir/scripts/pre-commit")
-    into("$rootDir/.git/hooks/")
 }
