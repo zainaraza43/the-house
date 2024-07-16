e284fcab4b4f5d438119d7a76758fefcacb289a4
zainaraza43 <urboyzain@gmail.com>
2024-07-15 22:56:42 -0400

    restarting database addition cus i am stupid

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,21 @@ repositories {
     }
 }
 
+val exposedVersion: String by project
 dependencies {
 
     implementation("dev.kord:kord-core:0.14.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.9.0-SNAPSHOT")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-money:$exposedVersion")
+    implementation("org.postgresql:postgresql:42.2.24")
 
     testImplementation(kotlin("test"))
 }
