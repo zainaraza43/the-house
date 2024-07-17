25394b157cc3a425d6f68ec610e2f554f617801b
zainaraza43 <urboyzain@gmail.com>
2024-07-17 19:46:54 -0400

    slash command added

--- the_house/main.py
+++ the_house/main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -15,12 +16,17 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
+    try:
+        await bot.tree.sync()
+    except Exception as e:
+        print(e)
     print(f'Bot is ready. Logged in as {bot.user}')
 
 
-@bot.command(name='ping')
-async def ping(ctx):
-    await ctx.send('Pong!')
+@bot.tree.command(name="add_league_of_legends_account", description="Add a League of Legends account")
+@app_commands.describe(username="The League of Legends username to add")
+async def add_league_of_legends_account(interaction: discord.Interaction, username: str):
+    await interaction.response.send_message(f'Account name "{username}" has been set.')
 
 
 if __name__ == '__main__':
